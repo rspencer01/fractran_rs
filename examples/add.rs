317454b9fefe96389b2c8a49f02608fe80457f85
Robert Spencer <general@robertandrewspencer.com>
2023-05-21 13:44:24 +0100

    docs: Add `add` example

---
+++ examples/add.rs
@@ -0,0 +1,24 @@
+use fractran_rs::*;
+
+struct AddInput(u32, u32);
+
+impl Into<usize> for AddInput {
+    fn into(self) -> usize {
+        2usize.pow(self.0) * 3usize.pow(self.1)
+    }
+}
+
+#[derive(Debug)]
+struct AddOutput(u32);
+
+impl From<usize> for AddOutput {
+    fn from(value: usize) -> Self {
+        AddOutput(value.trailing_zeros())
+    }
+}
+
+const ADD: FractranProgram<AddInput, AddOutput> = fractran!(2 / 3);
+
+fn main() {
+    println!("{:?}", ADD.run(AddInput(23, 17)));
+}
